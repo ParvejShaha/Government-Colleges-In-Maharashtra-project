04abba9dd8647b7c812d2f64985bda274c24fa24
ParvejShaha <145579675+ParvejShaha@users.noreply.github.com>
2023-11-01 12:24:55 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,117 @@
+<?php 
+
+session_start();
+
+	include("connection.php");
+	include("functions.php");
+
+
+	if($_SERVER['REQUEST_METHOD'] == "POST")
+	{
+		//something was posted
+		$user_name = $_POST['user_name'];
+		$password = $_POST['password'];
+
+		if(!empty($user_name) && !empty($password) && !is_numeric($user_name))
+		{
+
+			//read from database
+			$query = "select * from users where user_name = '$user_name' limit 1";
+			$result = mysqli_query($con, $query);
+
+			if($result)
+			{
+				if($result && mysqli_num_rows($result) > 0)
+				{
+
+					$user_data = mysqli_fetch_assoc($result);
+					
+					if($user_data['password'] === $password)
+					{
+
+						$_SESSION['user_id'] = $user_data['user_id'];
+						header("Location: index.html");
+						die;
+					}
+				}
+			}
+			
+			echo "wrong username or password!";
+		}else
+		{
+			echo "wrong username or password!";
+		}
+	}
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login</title>
+</head>
+<body>
+
+	<style type="text/css">
+	
+	body
+	{
+		background-color: lightblue ;
+	}
+	#text{
+
+		height: 25px;
+		border-radius: 5px;
+		padding: 4px;
+		border: solid thin #aaa;
+		width: 100%;
+	}
+
+	#button{
+
+		padding: 10px;
+		width: 100px;
+		color: white;
+		background-color: black;
+		border: none;
+
+	}
+
+	#box{
+
+		background-color: grey;
+		margin:auto;
+        width: 60%;
+        border-style:groove ; 
+        border-color:blue;
+        border-width: 4px;  
+        border-radius: 10px;
+        padding: 10px;
+        font-size: large;
+        color: white;
+	} 
+	h1{
+		text-align:center;
+		margin-top:200px;
+	
+	}
+
+	</style>
+	<h1>Welcome  Please Login</h1>
+
+	<div id="box">
+		
+		<form method="post">
+			<div style="font-size: 20px;margin: 10px;color: white;">Login</div>
+
+			<input id="text" type="text" name="user_name">Enter User Name<br><br>
+			<input id="text" type="password" name="password"><br>Enter Password<br><br>
+
+			<input id="button" type="submit" value="Login"><br><br>
+
+			<a href="signup.php">Click to Signup</a><br><br>
+		</form>
+	</div>
+</body>
+</html>
---
+++ signup.php
@@ -0,0 +1,102 @@
+<?php 
+session_start();
+
+	include("connection.php");
+	include("functions.php");
+
+
+	if($_SERVER['REQUEST_METHOD'] == "POST")
+	{
+		//something was posted
+		$user_name = $_POST['user_name'];
+		$password = $_POST['password'];
+
+		if(!empty($user_name) && !empty($password) && !is_numeric($user_name))
+		{
+
+			//save to database
+			$user_id = random_num(20);
+			$query = "insert into users (user_id,user_name,password) values ('$user_id','$user_name','$password')";
+
+			mysqli_query($con, $query);
+
+			header("Location: login.php");
+			die;
+		}else
+		{
+			echo "Please enter some valid information!";
+		}
+	}
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Signup</title>
+</head>
+<body>
+
+	<style type="text/css">
+
+    body
+	{
+		background-color: lightblue ;
+	}	
+	
+	#text{
+
+		height: 25px;
+		border-radius: 5px;
+		padding: 4px;
+		border: solid thin #aaa;
+		width: 100%;
+	}
+
+	#button{
+
+		padding: 10px;
+		width: 100px;
+		color: white;
+		background-color:black;
+		border: none;
+	}
+
+	#box{
+
+background-color: grey;
+margin:auto;
+width: 60%;
+border-style:groove ; 
+border-color:blue;
+border-width: 4px;  
+border-radius: 10px;
+padding: 10px;
+font-size: large;
+color: white;
+} 
+
+	h1{
+		text-align:center;
+		margin-top:200px;
+	
+	}
+
+	</style>
+	<h1>Welcome Please SignUp </h1>
+
+	<div id="box">
+		
+		<form method="post">
+			<div style="font-size: 20px;margin: 10px;color: white;">Signup</div>
+
+			<input id="text" type="text" name="user_name">Enter User Name<br><br>
+			<input id="text" type="password" name="password">Password<br><br>
+
+			<input id="button" type="submit" value="Signup"><br><br>
+
+			<a href="login.php">Click to Login</a><br><br>
+		</form>
+	</div>
+</body>
+</html>
